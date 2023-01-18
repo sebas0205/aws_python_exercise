70f2a8e794f040494cd14f3cbc45e2a318ac7548
Juan Sebastian Sanchez Hernandez <sebas0205@gmail.com>
2023-01-18 17:21:26 +1000

    print names

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ s3 = Services("s3")
 
 def get_buckets():
     response = s3.client.list_buckets()
-    print(response)
+    for bucket in response['Buckets']:
+        print(bucket['Name'])
+
 
 
 get_buckets()
